d340ef52013da0d9f5de5ede827edf84dc4cdf2a
Abed Hasan <abedhasan79@gmail.com>
2025-05-15 12:03:24 -0300

    working email

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -5,17 +5,17 @@ import { DateRangePicker } from 'rsuite';
 import 'rsuite/DateRangePicker/styles/index.css';
 import emailjs from '@emailjs/browser';
 
-const {  beforeToday } =
-  DateRangePicker;
+const { beforeToday } =
+    DateRangePicker;
 
 const BookingForm = ({ bike }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
         numberOfBikes: 1,
-        bikeId: bike?.id || '',
         startDateTime: '',
-        endDateTime: ''
+        endDateTime: '',
+
     });
 
     const handleChange = (e) => {
@@ -38,78 +38,84 @@ const BookingForm = ({ bike }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        // console.log(formData);
+
         emailjs.sendForm('service_urm4vnl', 'template_tljn269', form.current, 'd54-UknTik_wnUOyp')
-        .then((result) => {
-            setFormSubmitted(true); // Set the form submission status to true
-        }, (error) => {
-            console.log(error.text);
-        });
+            .then((result) => {
+                // console.log(result);
+                setFormSubmitted(true);
+            }, (error) => {
+                console.log(error.text);
+            });
     };
 
     return (
         <div className="container my-5">
             <h2 className="mb-4">Book Bicycle for a Day or More</h2>
-            <form  ref={form} onSubmit={handleSubmit} className="border p-4 rounded shadow-sm bg-light">
-            {formSubmitted ? (
+            <form ref={form} onSubmit={handleSubmit} className="border p-4 rounded shadow-sm bg-light">
+                {formSubmitted ? (
 
-                 <p> Thank you for your message! I will get back to you soon.</p>
-            
-            ) : (
-                <div>
-                    <div className="mb-3">
-                        <label className="form-label">Your Name</label>
-                        <input
-                            type="text"
-                            name="name"
-                            className="form-control"
-                            value={formData.name}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
+                    <p> Thank you for your message! I will get back to you soon.</p>
 
-                    <div className="mb-3">
-                        <label className="form-label">Email Address</label>
-                        <input
-                            type="email"
-                            name="email"
-                            className="form-control"
-                            value={formData.email}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">Number of Bikes</label>
-                        <select
-                            name="numberOfBikes"
-                            className="form-select"
-                            value={formData.numberOfBikes}
-                            onChange={handleChange}
-                            required
-                        >
-                            <option value={1}>1</option>
-                            <option value={2}>2</option>
-                        </select>
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">Please select booking <b>date and time</b>:</label>
-                        <>
-                            <DateRangePicker shouldDisableDate={beforeToday()} 
-                                format="MM/dd/yyyy HH:mm" 
-                                block
-                                onChange={handleDateRangeChange}
-                                placeholder="Select date and time range"
-                                name='date'
-                                value={formData.date}
+                ) : (
+                    <div>
+                        <div className="mb-3">
+                            <label className="form-label">Your Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                className="form-control"
+                                value={formData.name}
+                                onChange={handleChange}
+                                required
                             />
-                        </>
-                    </div>
+                        </div>
 
-                    <button type="submit" className="btn btn-success">Book Now</button>
-                </div>
+                        <div className="mb-3">
+                            <label className="form-label">Email Address</label>
+                            <input
+                                type="email"
+                                name="email"
+                                className="form-control"
+                                value={formData.email}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="form-label">Number of Bikes</label>
+                            <select
+                                name="numberOfBikes"
+                                className="form-select"
+                                value={formData.numberOfBikes}
+                                onChange={handleChange}
+                                required
+                            >
+                                <option value={1}>1</option>
+                                <option value={2}>2</option>
+                            </select>
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="form-label">Please select booking <b>date and time</b>:</label>
+                            <>
+                                <DateRangePicker shouldDisableDate={beforeToday()}
+                                    format="MM/dd/yyyy HH:mm"
+                                    block
+                                    onChange={handleDateRangeChange}
+                                    placeholder="Select date and time range"
+
+                                />
+                            </>
+
+                            {/* Hidden inputs to include in email */}
+                            <input type="hidden" name="startDateTime" value={formData.startDateTime} />
+                            <input type="hidden" name="endDateTime" value={formData.endDateTime} />
+                        </div>
+
+                        <button type="submit" className="btn btn-success">Book Now</button>
+                    </div>
                 )}
             </form>
 
